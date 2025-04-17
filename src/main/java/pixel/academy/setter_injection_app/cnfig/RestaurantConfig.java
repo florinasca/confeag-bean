f3b75095119487415f99c4e1d82036cf884e1814
florin <florinasca@mail.ru>
2025-04-17 20:33:20 +0300

    Initial commit.

---
+++ src/main/java/pixel/academy/setter_injection_app/cnfig/RestaurantConfig.java
@@ -0,0 +1,10 @@
+package pixel.academy.setter_injection_app.cnfig;
+
+import org.springframework.context.annotation.Configuration;
+
+public class RestaurantConfig {
+}
+
+
+@Configuration
+public class ResturantConfig
